2062c2f20d44f22e0e90a9ebce3e9ef00d420a9e
HackThreeDee <93230375+Hac3d@users.noreply.github.com>
2024-05-26 01:35:19 -0400

    orange

--- sys/modules/opus/ui.lua
+++ sys/modules/opus/ui.lua
@@ -39,7 +39,7 @@ function UI:init()
 	self.devices = { }
 	self.theme = {
 		colors = {
-			primary = colors.green,
+			primary = colors.orange,
 			secondary = colors.lightGray,
 			tertiary = colors.gray,
 		}
